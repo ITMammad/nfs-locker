d42431c7b50700691f79d125615a21f2af3e26c7
ITMammad <ITMammad.Coder@gmail.com>
2023-09-11 05:23:23 +0330

    Update Code.

--- funcs.js
+++ funcs.js
@@ -7,17 +7,8 @@ module.exports = {
         console.clear();
         console.log(`1. Encrypting ${input} With Password...`);
         const passwordHash = crypto.createHash("sha512").update(password).digest("hex");
-        const hashLength = 128;
+        const passwordHashBytes = Buffer.from(passwordHash).toJSON().data;
         const inputContents = fs.readFileSync(input);
-        const inputContentsLength = inputContents.length;
-        const repeats = (inputContentsLength - (inputContentsLength % hashLength)) / hashLength;
-        var repeatsString = "";
-        for (let i = 0; i < repeats; i++) {
-            repeatsString += passwordHash;
-        }
-        const overflow = inputContentsLength % hashLength;
-        const overflowString = passwordHash.substring(0, overflow);
-        const lengthedHash = Buffer.from(repeatsString + overflowString).toJSON().data;
         console.log(`2. Write Data To Output Path Provided.`);
         fs.writeFileSync(output, "");
         for (let i = 0; i < inputContents.length / 10485760; i++) {
@@ -27,7 +18,7 @@ module.exports = {
             console.log(`[${this.bar[i % 8]}] Part ${i + 1}/${((inputContents.length - (inputContents.length % 10485760)) / 10485760) + (inputContents.length % 10485760 === 0 ? 0 : 1)}`);
             var outputContentBytes = [];
             Buffer.from(inputContents).slice(i * 10485760, (i + 1) * 10485760).toJSON().data.forEach((byte, index) => {
-                outputContentBytes.push(byte + lengthedHash[index]);
+                outputContentBytes.push(byte + passwordHashBytes[index % 128]);
             });
             fs.writeFileSync(output, Buffer.concat([fs.readFileSync(output), Buffer.from(outputContentBytes)]))
         }
@@ -40,17 +31,8 @@ module.exports = {
         console.clear();
         console.log(`1. Decrypting ${input} With Password...`);
         const passwordHash = crypto.createHash("sha512").update(password).digest("hex");
-        const hashLength = 128;
+        const passwordHashBytes = Buffer.from(passwordHash).toJSON().data;
         const inputContents = fs.readFileSync(input);
-        const inputContentsLength = inputContents.length;
-        const repeats = (inputContentsLength - (inputContentsLength % hashLength)) / hashLength;
-        var repeatsString = "";
-        for (let i = 0; i < repeats; i++) {
-            repeatsString += passwordHash;
-        }
-        const overflow = inputContentsLength % hashLength;
-        const overflowString = passwordHash.substring(0, overflow);
-        const lengthedHash = Buffer.from(repeatsString + overflowString).toJSON().data;
         console.log(`2. Write Data To Output Path Provided.`);
         fs.writeFileSync(output, "");
         for (let i = 0; i < inputContents.length / 10485760; i++) {
@@ -60,7 +42,7 @@ module.exports = {
             console.log(`[${this.bar[i % 8]}] Part ${i + 1}/${((inputContents.length - (inputContents.length % 10485760)) / 10485760) + (inputContents.length % 10485760 === 0 ? 0 : 1)}`);
             var outputContentBytes = [];
             Buffer.from(inputContents).slice(i * 10485760, (i + 1) * 10485760).toJSON().data.forEach((byte, index) => {
-                outputContentBytes.push(byte - lengthedHash[index]);
+                outputContentBytes.push(byte - passwordHashBytes[index % 128]);
             });
             fs.writeFileSync(output, Buffer.concat([fs.readFileSync(output), Buffer.from(outputContentBytes)]))
         }
